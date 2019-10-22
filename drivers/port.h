dd9c7871eba7a685c2d32efa98a9a4ce62451405
Asher Mancinelli <ashermancinelli@gmail.com>
2019-10-22 21:45:48 +0000

    Added drivers
    Allows caller to read/write from a port.

---
+++ drivers/port.h
@@ -0,0 +1,4 @@
+extern unsigned char port_byte_in (unsigned char port);
+extern void port_byte_out (unsigned short port, unsigned char data);
+extern unsigned char port_word_in (unsigned char port);
+extern void port_word_out (unsigned char port, unsigned char data);
