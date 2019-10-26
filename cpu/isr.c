994d9af1c6fa96e5bdc51058354b3ddf8d48d539
Asher Mancinelli <ashermancinelli@gmail.com>
2019-10-26 19:52:43 +0000

    partially through installing irqs

--- cpu/isr.c
+++ cpu/isr.c
@@ -12,7 +12,25 @@
  * handlers defined in `interrupt.asm`, so make sure to
  * link that object file manually.
  *
+ * PIC: PIC controls the CPU's interrupt mechanism, by 
+ * accepting several interrupt requests and feeding them 
+ * to the processor in order. Each chip is comprised of a 
+ * master and slave, and each part has a data port and
+ * a command port.
+ *
+ *  -------------------- --------
+ * | master pic command | 0x0020 |
+ * | master pic data    | 0x0021 |
+ * | slave pic command  | 0x00A0 |
+ * | slave pic data     | 0x00A1 |
+ *  -------------------- --------
+ *
+ *  See the 'Remap the PIC' comment for the application
+ *  of this table.
+ *
  */
+
+isr_t interrupt_handlers[256];
 
 /*
  * Cant do in a loop because we
@@ -52,6 +70,36 @@ void isr_install()
     set_idt_gate(29, (uint32_t)isr29);
     set_idt_gate(30, (uint32_t)isr30);
     set_idt_gate(31, (uint32_t)isr31);
+
+    // Remap the PIC
+    port_byte_out(0x20, 0x11);
+    port_byte_out(0xA0, 0x11);
+    port_byte_out(0x21, 0x20);
+    port_byte_out(0xA1, 0x28);
+    port_byte_out(0x21, 0x04);
+    port_byte_out(0xA1, 0x02);
+    port_byte_out(0x21, 0x01);
+    port_byte_out(0xA1, 0x01);
+    port_byte_out(0x21, 0x0);
+    port_byte_out(0xA1, 0x0);
+
+    // Install the IRQs
+    set_idt_gate(32, (u32)irq0);
+    set_idt_gate(33, (u32)irq1);
+    set_idt_gate(34, (u32)irq2);
+    set_idt_gate(35, (u32)irq3);
+    set_idt_gate(36, (u32)irq4);
+    set_idt_gate(37, (u32)irq5);
+    set_idt_gate(38, (u32)irq6);
+    set_idt_gate(39, (u32)irq7);
+    set_idt_gate(40, (u32)irq8);
+    set_idt_gate(41, (u32)irq9);
+    set_idt_gate(42, (u32)irq10);
+    set_idt_gate(43, (u32)irq11);
+    set_idt_gate(44, (u32)irq12);
+    set_idt_gate(45, (u32)irq13);
+    set_idt_gate(46, (u32)irq14);
+    set_idt_gate(47, (u32)irq15);
 
     set_idt(); // Load with ASM
 }
